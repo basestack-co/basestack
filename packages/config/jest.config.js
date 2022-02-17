182e24f4042a2afc3bfeec4364eab6ad2c63190c
Vitor Amaral <vitor.works@gmail.com>
2022-02-17 18:56:30 +0000

    chore: update test config and upgrade nextjs version to 12.1

--- packages/config/jest.config.js
+++ packages/config/jest.config.js
@@ -7,13 +7,10 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-
-    "^@/pages/(.*)$": "<rootDir>/pages/$1",
-  },
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 };
 
