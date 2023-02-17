54154e63678ca9e34ec3698ff2290586728fb436
Vitor Amaral <vitor.works@hotmail.com>
2023-02-17 20:14:19 +0000

    T-100 - prepare for the early-access page

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -2,6 +2,15 @@ const withTM = require("next-transpile-modules")(["@basestack/design-system"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/ZVDXLgW6RVWuzB",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
   /* typescript: {
     ignoreBuildErrors: true,
   }, */
