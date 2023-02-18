4ee7a27ca9f729eba14fabf65a4bbfa8e64e41b8
Vitor Amaral <vitor.works@hotmail.com>
2023-02-18 10:21:02 +0000

    T-100: update wait list landing page with zod and form

--- apps/landing-page/components/WaitingList/index.tsx
+++ apps/landing-page/components/WaitingList/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { rem } from "polished";
 import { useTheme } from "styled-components";
+// Form
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+// Utils
+import { z } from "zod";
 // Components
 import Image from "../Image";
 import Logo from "../Logo";
@@ -17,8 +22,31 @@ import {
   Footer,
 } from "./styles";
 
+export const FormSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Required field")
+    .email("This is not a valid email."),
+});
+
+export type FormInputs = z.TypeOf<typeof FormSchema>;
+
 const WaitingList = () => {
   const theme = useTheme();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<FormInputs>({
+    resolver: zodResolver(FormSchema),
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<FormInputs> = (input: FormInputs) => {
+    console.log(input);
+  };
 
   return (
     <Container>
@@ -38,7 +66,7 @@ const WaitingList = () => {
               color={theme.colors.black}
               mb={theme.spacing.s5}
             >
-              Feature Flag Service
+              The Essential Stack for Developers and Startups
             </Text>
             <Text
               size="xxLarge"
@@ -48,18 +76,35 @@ const WaitingList = () => {
               as="p"
               color={theme.colors.gray500}
             >
-              Release features with confidence, manage feature flags across web,
-              mobile, and server side applications.
+              Feature Flags are just the beginning: Our Suite of Tools will
+              include Feedback, Forms, and more to help you build better
+              Products!
             </Text>
             <InputContainer>
-              <Input placeholder="Enter your email" />
+              <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Input
+                    placeholder="Enter your email"
+                    type="email"
+                    name={field.name}
+                    value={field.value}
+                    onChange={field.onChange}
+                    onBlur={field.onBlur}
+                  />
+                )}
+              />
               <Button
-                onClick={() => console.log("yeah")}
+                onClick={handleSubmit(onSubmit)}
+                isDisabled={isSubmitting}
+                isLoading={isSubmitting}
                 size={ButtonSize.Medium}
                 flexShrink={0}
                 icon="arrow_forward"
               >
-                Join the waitlist
+                Get Early Access
               </Button>
             </InputContainer>
           </TextContainer>
@@ -72,7 +117,7 @@ const WaitingList = () => {
         </Grid>
         <Footer>
           <Text size="medium" fontWeight={400} muted>
-            © MoonFlags 2023. All rights reserved.
+            © Basestack {new Date().getFullYear()}. All rights reserved.
           </Text>
         </Footer>
       </ContentContainer>
