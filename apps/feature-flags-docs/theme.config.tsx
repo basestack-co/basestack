83254b987cd975c38d81672cb79f4d81f5b822b3
Vitor Amaral <vitor.works@hotmail.com>
2023-07-13 15:19:24 +0100

    chore: sync progress on docs structure

--- apps/feature-flags-docs/theme.config.tsx
+++ apps/feature-flags-docs/theme.config.tsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+// Utils
+import { repoUrl } from "utils/helpers";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Basestack</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: repoUrl,
   },
-  chat: {
-    link: "https://discord.com",
-  },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: `${repoUrl}/blob/master/apps/feature-flags-docs/pages`,
   footer: {
-    text: "Nextra Docs Template",
+    text: "Basestack Docs",
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Basestack Docs",
+    };
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Basestack Docs" />
+      <meta
+        property="og:description"
+        content="The official Basestack documentation"
+      />
+    </>
+  ),
 };
 
 export default config;
