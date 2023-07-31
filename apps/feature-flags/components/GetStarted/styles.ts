ea0e6c2898168bcd35562d09909c279f3912c7bb
flavio amaral <fara-16@hotmail.com>
2023-07-31 18:42:38 +0100

    style(update start page): update styling of start page

--- apps/feature-flags/components/GetStarted/styles.ts
+++ apps/feature-flags/components/GetStarted/styles.ts
@@ -1,29 +1,21 @@
 import styled from "styled-components";
-import { rem } from "polished";
-import { Card } from "@basestack/design-system";
+import { scrollbar } from "@basestack/design-system/styles";
 
 export const Container = styled.main`
   padding: ${({ theme }) => theme.spacing.s6} ${({ theme }) => theme.spacing.s5};
-  max-width: 1200px;
+  max-width: 992px;
   margin: 0 auto;
   width: 100%;
 `;
 
 export const ContentContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 400px;
-  align-items: start;
-  grid-gap: ${({ theme }) => theme.spacing.s5};
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.s5};
 
   @media screen and ${({ theme }) => theme.device.max.md} {
     grid-template-columns: 1fr;
   }
-`;
-
-export const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: ${({ theme }) => theme.spacing.s5};
 `;
 
 export const StyledLink = styled.a`
@@ -34,5 +26,35 @@ export const StyledLink = styled.a`
 
   &:hover {
     text-decoration: underline;
+  }
+`;
+
+export const ProjectsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.s3};
+  max-height: 244px;
+  overflow: auto;
+  ${scrollbar};
+`;
+
+export const ProjectsListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProjectButton = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  padding: ${({ theme }) => theme.spacing.s3};
+  background-color: ${({ theme }) =>
+    theme.colors[theme.isDarkMode ? "gray700" : "gray50"]};
+
+  &:hover:not(:active) {
+    cursor: pointer;
+    background-color: ${({ theme }) =>
+      theme.colors[theme.isDarkMode ? "gray600" : "gray100"]};
   }
 `;
--- apps/feature-flags/pages/index.tsx
+++ apps/feature-flags/pages/index.tsx
@@ -1,10 +1,16 @@
+import { useCallback, useMemo } from "react";
+import { useTheme } from "styled-components";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useStore } from "store";
+import { trpc } from "libs/trpc";
+import { useSession } from "next-auth/react";
 // Components
 import {
   Text,
-  HorizontalRule,
   ButtonVariant,
   Card,
+  Avatar,
   IconBox,
 } from "@basestack/design-system";
 import GetStartedCard from "components/GetStarted/GetStartedCard";
@@ -13,14 +19,54 @@ import TextLink from "components/GetStarted/TextLink";
 import {
   Container,
   ContentContainer,
-  ContentWrapper,
+  ProjectButton,
+  ProjectsList,
+  ProjectsListItem,
 } from "components/GetStarted/styles";
-import { useTheme } from "styled-components";
 // Layout
 import MainLayout from "../layouts/Main";
 
 const MainPage = () => {
+  const router = useRouter();
   const theme = useTheme();
+
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/sign-in");
+    },
+  });
+
+  const { data } = trpc.project.all.useQuery(undefined, {
+    enabled: status === "authenticated",
+  });
+
+  const setCreateProjectModalOpen = useStore(
+    (state) => state.setCreateProjectModalOpen,
+  );
+
+  const onSelectProject = useCallback(
+    (projectSlug: string) => {
+      router.push({
+        pathname: "/[projectSlug]/flags",
+        query: { projectSlug },
+      });
+    },
+    [router],
+  );
+
+  const projects = useMemo(
+    () =>
+      data?.projects.map((item) => {
+        return {
+          id: item.id,
+          slug: item.slug,
+          onClick: () => onSelectProject(item.slug),
+          text: item.name,
+        };
+      }),
+    [data, onSelectProject],
+  );
 
   return (
     <div>
@@ -31,50 +77,61 @@ const MainPage = () => {
       <Container>
         <Text size="xLarge">Let’s get started</Text>
         <Text muted size="small" mb={theme.spacing.s5}>
-          Follow the steps below to start using Moonflags
+          Follow the steps below to start
         </Text>
         <ContentContainer>
-          <ContentWrapper>
+          {projects?.length && (
+            <Card hasHoverAnimation p={theme.spacing.s5}>
+              <Text size="large" mb={theme.spacing.s5}>
+                Recent Projects
+              </Text>
+              <ProjectsList>
+                {projects?.map((project) => (
+                  <ProjectsListItem key={project.id}>
+                    <ProjectButton onClick={project.onClick}>
+                      <Avatar
+                        size="small"
+                        userName={project.text}
+                        alt={`${project.text} project`}
+                        round={false}
+                      />
+                      <Text size="small" ml={theme.spacing.s2}>
+                        {project.text}
+                      </Text>
+                    </ProjectButton>
+                  </ProjectsListItem>
+                ))}
+              </ProjectsList>
+            </Card>
+          )}
+          {!projects?.length && (
             <GetStartedCard
               icon={{
-                name: "downloading",
-                color: "green",
-              }}
-              title="Install our SDK"
-              description="Integrate Moonflags into your Javascript, React, Go, PHP. More languages are coming soon!"
-              button={{
-                text: "View Instructions",
-                onClick: () => console.log("instructions"),
-                variant: ButtonVariant.Tertiary,
-              }}
-            />
-            <GetStartedCard
-              icon={{
-                name: "flag",
+                name: "folder_open",
                 color: "blue",
               }}
-              title="Create your first feature"
-              description="Create a feature within Moonflags. Use features to toggle app behavior, do gradual rollouts, and run A/B testing."
+              title="Create your first project"
+              description="Create your first project to start creating flags!"
               button={{
-                text: "Create Feature Flag",
-                onClick: () => console.log("instructions"),
+                text: "Create Project",
+                onClick: () => setCreateProjectModalOpen({ isOpen: true }),
                 variant: ButtonVariant.Primary,
               }}
             />
-            <GetStartedCard
-              icon={{
-                name: "groups",
-                color: "purple",
-              }}
-              title="Invite team"
-              description="Invite teammates to your account."
-              button={{
-                text: "Invite Team",
-                onClick: () => console.log("instructions"),
-                variant: ButtonVariant.Tertiary,
-              }}
-            />
-          </ContentWrapper>
+          )}
+          <GetStartedCard
+            icon={{
+              name: "downloading",
+              color: "green",
+            }}
+            title="Install our SDK"
+            description="Integrate Moonflags into your Javascript, React, Go, PHP. More languages are coming soon!"
+            button={{
+              text: "View Instructions",
+              onClick: () => console.log("instructions"),
+              variant: ButtonVariant.Tertiary,
+            }}
+          />
           <Card hasHoverAnimation p={theme.spacing.s5}>
             <IconBox icon="folder_open" color="gray" />
             <Text size="large" mb={theme.spacing.s2}>
@@ -95,25 +152,6 @@ const MainPage = () => {
             <TextLink
               text=" Open an issue on"
               link={{ text: "Github", href: "/" }}
-              hasMarginBottom={false}
-            />
-            <HorizontalRule my={theme.spacing.s5} />
-            <Text size="large" mb={theme.spacing.s2}>
-              Other Links
-            </Text>
-            <TextLink text="Join us on" link={{ text: "Slack", href: "/" }} />
-            <TextLink
-              text="Read our"
-              link={{ text: "Docs", href: "/" }}
-              hasMarginBottom={false}
-            />
-            <HorizontalRule my={theme.spacing.s5} />
-            <Text size="large" mb={theme.spacing.s2}>
-              Have questions?
-            </Text>
-            <TextLink
-              text="Talk to us in our"
-              link={{ text: "Slack channel", href: "/" }}
               hasMarginBottom={false}
             />
           </Card>
