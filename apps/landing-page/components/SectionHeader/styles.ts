42d67b6656587a30eb23e4d94dfa5270881fc478
Vitor Amaral <vitor.works@hotmail.com>
2023-08-15 16:26:20 +0100

    chore: fix email endpoint and title for landing page styles

--- apps/landing-page/components/SectionHeader/styles.ts
+++ apps/landing-page/components/SectionHeader/styles.ts
@@ -1,7 +1,13 @@
 import styled, { css, keyframes } from "styled-components";
 import { rem } from "polished";
-import { space, typography } from "styled-system";
-import { Text } from "@basestack/design-system";
+import {
+  space,
+  typography,
+  color,
+  SpaceProps,
+  ColorProps,
+  TypographyProps,
+} from "styled-system";
 
 export const Container = styled.div<{ hasMarginBottom: boolean }>`
   display: flex;
@@ -25,30 +31,37 @@ const animateText = keyframes`
   }
 `;
 
-interface AnimatedTextProps {
+interface TitleProps extends SpaceProps, ColorProps, TypographyProps {
   titleSize: "normal" | "large";
   as: string;
+  hasAnimatedText: boolean;
 }
 
-export const AnimatedText = styled(Text)<AnimatedTextProps>`
+export const Title = styled.h2<TitleProps>`
   ${typography};
   ${space};
-  background: linear-gradient(
-    to right,
-    ${({ theme }) => theme.colors.black} 20%,
-    ${({ theme }) => theme.colors.blue500} 30%,
-    ${({ theme }) => theme.colors.blue300} 70%,
-    ${({ theme }) => theme.colors.purple500} 80%
-  );
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  text-fill-color: transparent;
-  background-size: 500% auto;
-  animation: ${animateText} 5s ease-in-out infinite alternate;
+  ${color};
   font-family: ${({ theme }) => theme.typography.robotoFlex};
   font-size: ${({ titleSize }) =>
     titleSize === "normal" ? rem("42px") : rem("60px")};
+
+  ${({ hasAnimatedText }) =>
+    hasAnimatedText &&
+    css`
+      background: linear-gradient(
+        to right,
+        ${({ theme }) => theme.colors.black} 20%,
+        ${({ theme }) => theme.colors.blue500} 30%,
+        ${({ theme }) => theme.colors.blue300} 70%,
+        ${({ theme }) => theme.colors.purple500} 80%
+      );
+      -webkit-background-clip: text;
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+      text-fill-color: transparent;
+      background-size: 500% auto;
+      animation: ${animateText} 5s ease-in-out infinite alternate;
+    `}
 
   @media screen and ${({ theme }) => theme.device.max.md} {
     font-size: ${rem("32px")};
