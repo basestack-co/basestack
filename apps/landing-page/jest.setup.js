f6ea07db43fa647776181a80b625b47830f0d3b0
Vitor Amaral <vitor.works@hotmail.com>
2023-08-16 19:17:24 +0100

    chore: fix testing suite config

--- apps/landing-page/jest.setup.js
+++ apps/landing-page/jest.setup.js
@@ -1,4 +1,5 @@
 const common = require("@basestack/config/jest.setup.js");
+
 require("jest-fetch-mock").enableMocks();
 
 module.exports = common;
--- packages/config/jest.base.config.js
+++ packages/config/jest.base.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   clearMocks: true,
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   coverageDirectory: "coverage",
   roots: ["<rootDir>"],
   testMatch: ["**/?(*.)+(spec).+(ts|tsx|js|jsx)"],
   transform: { "^.+\\.(ts|tsx|jsx)$": "ts-jest" },
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   modulePathIgnorePatterns: ["/dist/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
