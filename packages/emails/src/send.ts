d755c55656e7d8311d5c6ac44a80a0c9c468d2c9
Vitor Amaral <vitor.works@hotmail.com>
2023-09-16 19:12:53 +0100

    feat: add config to send email via smtp

--- packages/emails/src/send.ts
+++ packages/emails/src/send.ts
@@ -2,26 +2,40 @@
 import nodemailer from "nodemailer";
 
 export interface SendEmailOptions {
+  from: string;
+  to: string;
+  subject: string;
+}
+export interface SendEmail {
   html: string;
+  options: SendEmailOptions;
 }
 
-export const sendEmail = async ({ html }: SendEmailOptions) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.forwardemail.net",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "my_user",
-      pass: "my_password",
-    },
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SMTP_HOST,
+  port: Number(process.env.EMAIL_SMTP_PORT) ?? 465,
+  secure: !!process.env.EMAIL_SMTP_SECURE ?? true,
+  auth: {
+    user: process.env.EMAIL_SMTP_AUTH_USER,
+    pass: process.env.EMAIL_SMTP_AUTH_PASSWORD,
+  },
+});
+
+export const sendEmail = async ({ html, options }: SendEmail) => {
+  if (!process.env.EMAIL_SMTP_HOST) {
+    throw new Error("EMAIL_SMTP_HOST is not defined");
+  }
+
+  // This code is needed to work with Next.js API routes
+  return await new Promise((resolve, reject) => {
+    transporter.sendMail({ ...options, html }, (error, info) => {
+      if (error) {
+        console.error("The email could not be sent, error = ", error);
+        reject(error);
+      } else {
+        console.log("The email was sent successfully, info = ", info);
+        resolve(info);
+      }
+    });
   });
-
-  const options = {
-    from: "you@example.com",
-    to: "user@gmail.com",
-    subject: "hello world",
-    html,
-  };
-
-  await transporter.sendMail(options);
 };
--- packages/emails/src/templates/email.tsx
+++ packages/emails/src/templates/email.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-// Email components
-import { Button } from "@react-email/button";
-import { Html } from "@react-email/html";
+// Components
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Button,
+} from "@react-email/components";
 
-export default function Email() {
-  return (
-    <Html>
-      <Button
-        pX={20}
-        pY={12}
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff" }}
-      >
-        Click me
-      </Button>
-    </Html>
-  );
+interface Props {
+  loginCode?: string;
 }
+
+const main = {
+  backgroundColor: "#ffffff",
+};
+
+const container = {
+  paddingLeft: "12px",
+  paddingRight: "12px",
+  margin: "0 auto",
+};
+
+export const Email = ({ loginCode = "" }: Props) => (
+  <Html>
+    <Head />
+    <Preview>Log in with this magic link</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Button href="https://example.com" style={{ color: "#61dafb" }}>
+          Click me
+        </Button>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default Email;
