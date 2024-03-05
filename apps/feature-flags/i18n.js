cb9aca000407ed9b9fe01996252198ccc0029f62
Vitor Amaral <vitor.works@gmail.com>
2024-03-05 19:41:25 +0000

    chore: move all locales to package folder

--- apps/feature-flags/i18n.js
+++ apps/feature-flags/i18n.js
@@ -1,4 +1,8 @@
 module.exports = {
+  loadLocaleFrom: (lang, ns) =>
+    import(`../../packages/locales/feature-flags/${lang}/${ns}.json`).then(
+      (m) => m.default,
+    ),
   locales: ["en"],
   defaultLocale: "en",
   pages: {
--- apps/forms/i18n.js
+++ apps/forms/i18n.js
@@ -1,4 +1,8 @@
 module.exports = {
+  loadLocaleFrom: (lang, ns) =>
+    import(`../../packages/locales/forms/${lang}/${ns}.json`).then(
+      (m) => m.default,
+    ),
   locales: ["en"],
   defaultLocale: "en",
   pages: {
