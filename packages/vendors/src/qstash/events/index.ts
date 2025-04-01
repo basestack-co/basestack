541376dfd70f01dcddb067e793fc233ce342cd9f
vacom <vitor.works@gmail.com>
2025-04-01 15:14:05 +0000

    chore: add workflow client

--- packages/vendors/src/qstash/events/index.ts
+++ packages/vendors/src/qstash/events/index.ts
@@ -9,27 +9,28 @@ import type {
 
 // AI JOBS
 
-const aiQueue = client.queue({
+/* const aiQueue = client.queue({
   queueName: "ai-queue",
-});
+}); */
 
 export const checkDataForSpamEvent = async (body: CheckDataForSpamPayload) => {
-  await aiQueue.enqueueJSON({
+  await client.trigger({
     url: `${baseUrl}/api/v1/jobs/check-spam`,
     retries: 2,
-    contentBasedDeduplication: true,
     body,
   });
 };
 
 // NOTIFICATIONS JOBS
 
-const notificationsQueue = client.queue({
+/* const notificationsQueue = client.queue({
   queueName: "notifications-queue",
-});
+}); */
+
+//  await notificationsQueue.enqueueJSON({
 
 export const sendEmailEvent = async (body: SendEmailPayload) => {
-  await notificationsQueue.enqueueJSON({
+  await client.trigger({
     url: `${baseUrl}/api/v1/jobs/send-email`,
     retries: 2,
     body,
@@ -39,7 +40,7 @@ export const sendEmailEvent = async (body: SendEmailPayload) => {
 export const sendDataToExternalWebhookEvent = async (
   body: SendDataToExternalWebhookPayload,
 ) => {
-  await notificationsQueue.enqueueJSON({
+  await client.trigger({
     url: `${baseUrl}/api/v1/jobs/send-data-to-external-webhook`,
     retries: 2,
     body,
