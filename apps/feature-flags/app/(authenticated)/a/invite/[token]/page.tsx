ca010c696d36bf0e69c82260f2610e134be6d117
amaralflavio <fara-16@hotmail.com>
2025-04-26 19:34:27 +0000

    progress

--- apps/feature-flags/app/(authenticated)/a/invite/[token]/page.tsx
+++ apps/feature-flags/app/(authenticated)/a/invite/[token]/page.tsx
@@ -12,7 +12,7 @@ import {
   Avatar,
   IconBox,
 } from "@basestack/design-system";
-import { Body, Buttons, Container, Footer, Header } from "./styles";
+import { Body, Buttons, Container, Footer, Header, Wrapper } from "./styles";
 
 const InvitePage = () => {
   const { spacing } = useTheme();
@@ -23,77 +23,89 @@ const InvitePage = () => {
 
   return (
     <Container>
-      <Card p={spacing.s5}>
-        <Header>
-          <Text size="small" muted>
-            Today, April 10 2025
-          </Text>
-          <Text size="xLarge" fontWeight={400} mt={spacing.s1}>
-            Pending Invite
-          </Text>
-        </Header>
+      <Wrapper>
+        <Card p={spacing.s5}>
+          <Header>
+            <Text size="small" muted>
+              Today, April 10 2025
+            </Text>
+            <Text size="xLarge" fontWeight={400} mt={spacing.s1}>
+              Pending Invite
+            </Text>
+          </Header>
 
-        <Body>
-          <Avatar src="" userName="Sandro Gee" alt="Sandro Gee" size="large" />
+          <Body>
+            <Avatar
+              src=""
+              userName="Sandro Gee"
+              alt="Sandro Gee"
+              size="large"
+            />
 
-          <Text
-            size="xxLarge"
-            fontWeight={400}
-            mt={spacing.s3}
-            textAlign="center"
-          >
-            You have being invited to join the team
-          </Text>
-          <Text size="xxLarge" fontWeight={500}>
-            {`"${token}"`}
-          </Text>
-
-          <IconBox icon="group_add" mt={spacing.s7} size="large" />
-          <Text
-            size="medium"
-            fontWeight={400}
-            muted
-            mt={spacing.s3}
-            textAlign="center"
-          >
-            6 from your team have already accepted
-          </Text>
-        </Body>
-
-        <Footer>
-          <Text size="small" fontWeight={400} muted>
-            Your invitation expires in 7 days
-          </Text>
-          <Buttons>
-            <Button variant={ButtonVariant.Outlined} onClick={() => undefined}>
-              Decline
-            </Button>
-            <Button
-              variant={ButtonVariant.Primary}
-              onClick={() =>
-                acceptInvitation.mutate(
-                  { token },
-                  {
-                    onSuccess: async () => {
-                      await trpcUtils.project.all.invalidate();
-                      await trpcUtils.project.recent.invalidate();
-
-                      toast.success("Successfully joined team, redirecting...");
-
-                      router.push("/a");
-                    },
-                    onError: (error) => {
-                      toast.error(error.message);
-                    },
-                  },
-                )
-              }
+            <Text
+              size="xxLarge"
+              fontWeight={400}
+              mt={spacing.s3}
+              textAlign="center"
             >
-              Accept Invitation
-            </Button>
-          </Buttons>
-        </Footer>
-      </Card>
+              You have being invited to join the team
+            </Text>
+            <Text size="xxLarge" fontWeight={500}>
+              {`"${token}"`}
+            </Text>
+
+            <IconBox icon="group_add" mt={spacing.s7} size="large" />
+            <Text
+              size="medium"
+              fontWeight={400}
+              muted
+              mt={spacing.s3}
+              textAlign="center"
+            >
+              6 from your team have already accepted
+            </Text>
+          </Body>
+
+          <Footer>
+            <Text size="small" fontWeight={400} muted>
+              Your invitation expires in 7 days
+            </Text>
+            <Buttons>
+              <Button
+                variant={ButtonVariant.Outlined}
+                onClick={() => undefined}
+              >
+                Decline
+              </Button>
+              <Button
+                variant={ButtonVariant.Primary}
+                onClick={() =>
+                  acceptInvitation.mutate(
+                    { token },
+                    {
+                      onSuccess: async () => {
+                        await trpcUtils.project.all.invalidate();
+                        await trpcUtils.project.recent.invalidate();
+
+                        toast.success(
+                          "Successfully joined team, redirecting...",
+                        );
+
+                        router.push("/a");
+                      },
+                      onError: (error) => {
+                        toast.error(error.message);
+                      },
+                    },
+                  )
+                }
+              >
+                Accept Invitation
+              </Button>
+            </Buttons>
+          </Footer>
+        </Card>
+      </Wrapper>
     </Container>
   );
 };
--- apps/feature-flags/app/(authenticated)/a/invite/[token]/styles.ts
+++ apps/feature-flags/app/(authenticated)/a/invite/[token]/styles.ts
@@ -1,10 +1,46 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  padding: ${({ theme }) => theme.spacing.s6} ${({ theme }) => theme.spacing.s5};
-  max-width: 616px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  padding: ${({ theme }) => theme.spacing.s8} ${({ theme }) => theme.spacing.s5};
+  width: 100%;
+  position: relative;
+
+  &::before {
+    content: "";
+    background: ${({ theme }) =>
+      theme.isDarkMode ? "rgba(20, 20, 20, 0.7)" : "rgba(246, 246, 246, 0.7)"};
+    -webkit-backdrop-filter: saturate(180%) blur(150px);
+    backdrop-filter: saturate(180%) blur(150px);
+    position: absolute;
+    z-index: -1;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+  }
+`;
+
+export const Wrapper = styled.main`
+  max-width: 576px;
   margin: 0 auto;
   width: 100%;
+  position: relative;
+
+  &::after {
+    content: "";
+    background: ${({ theme }) => theme.colors.primary};
+    opacity: 0.5;
+    position: absolute;
+    border-radius: 50%;
+    z-index: -2;
+    height: 250px;
+    width: 250px;
+    left: calc(50% - 125px);
+    top: 0;
+  }
 `;
 
 export const Header = styled.div`
