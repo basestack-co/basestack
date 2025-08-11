74af214d23ddd06efa0ce3bf3b98acbfd18a2b36
vacom <vitor.works@gmail.com>
2025-08-11 12:33:38 +0000

    chore: Remove unused environment variables and Prisma client code from feature flags app

--- apps/feature-flags/server/db/index.ts
+++ apps/feature-flags/server/db/index.ts
@@ -1,19 +1,3 @@
-// Types
-/* import { PrismaClient } from ".prisma/client";
-
-const createPrismaClient = () =>
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; */
-
 // React
 import { cache } from "react";
 // Prisma
